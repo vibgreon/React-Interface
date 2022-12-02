6dc4bde8a32503b2ce772541d46a3e5cbd48d7bc
vibgreon <vivekvenkatesh1234@gmail.com>
2022-12-03 00:10:13 +0530

    installed reacticons tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BiAccessibility } from 'react-icons/bi'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className='text-5xl'>
+        <BiAccessibility />Your Appointments</h1>
     </div>
   );
 }
