f56fb7fb81b9d305ff3bec6fc6c1003e6a7400b1
vibgreon <vivekvenkatesh1234@gmail.com>
2022-12-03 13:49:32 +0530

    adding components and data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { BiAccessibility } from 'react-icons/bi'
+import { BiCalendar } from 'react-icons/bi'
+import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
+import appointmentList from "./data.json"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className='text-5xl'>
-        <BiAccessibility />Your Appointments</h1>
+      <h1 className='text-5xl mb-3'>
+        <BiCalendar className='inline-block text-red-400 align-top' />Your Appointments</h1>
+      <AddAppointment />
+      <Search />
+
+      <ul className='divide-y divide-gray-200'>
+        {appointmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id}
+            appointment = {appointment} />
+        ))}
+      </ul>
     </div>
   );
 }
